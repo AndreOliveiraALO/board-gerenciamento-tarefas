01c3359b33e437f5e472dcc7a76555203f6ba323
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-08-31 20:33:37 -0300

    criação da rotina para testes na app e arquivos xml com as tabelas

--- boardtarefas/src/main/java/com/dio/santander/App.java
+++ boardtarefas/src/main/java/com/dio/santander/App.java
@@ -2,10 +2,16 @@ package com.dio.santander;
 
 import java.sql.SQLException;
 
+import static com.dio.santander.persistence.config.ConnectionConfig.getConnection;
+import com.dio.santander.persistence.migration.MigrationStrategy;
+
 public class App 
 {
     public static void main( String[] args ) throws SQLException
     {
-        
+        try(var connection = getConnection()){
+            new MigrationStrategy(connection).executeMigration();
+            System.out.println("Migrações executadas com sucesso!");
+        }  
     }
 }
