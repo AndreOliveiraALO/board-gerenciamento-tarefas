9ed0fe3db2c2d7471a563c3930a834410a09ff8f
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-08-31 22:23:28 -0300

    Criação das classes de dominios

---
+++ boardtarefas/src/main/java/com/dio/santander/persistence/entity/BoardColumnEntity.java
@@ -0,0 +1,68 @@
+package com.dio.santander.persistence.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardColumnEntity {
+
+    private Long id;
+    private String name;
+    private int order;
+    private BoardColumnKindEnum kind;
+    private BoardEntity board = new BoardEntity();
+    //@ToString.Exclude
+    //@EqualsAndHashCode.Exclude
+    private List<CardEntity> cards = new ArrayList<>();
+   
+    public BoardColumnEntity() {
+    }   
+
+    public BoardColumnEntity(Long id, String name, int order, BoardColumnKindEnum kind, BoardEntity board,
+            List<CardEntity> cards) {
+        this.id = id;
+        this.name = name;
+        this.order = order;
+        this.kind = kind;
+        this.board = board;
+        this.cards = cards;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getOrder() {
+        return order;
+    }
+    public void setOrder(int order) {
+        this.order = order;
+    }
+    public BoardColumnKindEnum getKind() {
+        return kind;
+    }
+    public void setKind(BoardColumnKindEnum kind) {
+        this.kind = kind;
+    }
+    public BoardEntity getBoard() {
+        return board;
+    }
+    public void setBoard(BoardEntity board) {
+        this.board = board;
+    }
+    public List<CardEntity> getCards() {
+        return cards;
+    }
+    public void setCards(List<CardEntity> cards) {
+        this.cards = cards;
+    }    
+
+}
+
---
+++ boardtarefas/src/main/java/com/dio/santander/persistence/entity/BoardEntity.java
@@ -0,0 +1,68 @@
+package com.dio.santander.persistence.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import static com.dio.santander.persistence.entity.BoardColumnKindEnum.CANCEL;
+import static com.dio.santander.persistence.entity.BoardColumnKindEnum.INITIAL;
+
+public class BoardEntity {
+
+    private Long id;
+    private String name;
+    //@ToString.Exclude
+    //@EqualsAndHashCode.Exclude
+    private List<BoardColumnEntity> boardColumns = new ArrayList<>();
+
+    public BoardColumnEntity getInitialColumn(){
+        return getFilteredColumn(bc -> bc.getKind().equals(INITIAL));
+    }
+
+    public BoardColumnEntity getCancelColumn(){
+        return getFilteredColumn(bc -> bc.getKind().equals(CANCEL));
+    }
+
+    private BoardColumnEntity getFilteredColumn(Predicate<BoardColumnEntity> filter){
+        return boardColumns.stream()
+                .filter(filter)
+                .findFirst().orElseThrow();
+    }
+
+    public BoardEntity() {
+    }
+
+    public BoardEntity(Long id, String name, List<BoardColumnEntity> boardColumns) {
+        this.id = id;
+        this.name = name;
+        this.boardColumns = boardColumns;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BoardColumnEntity> getBoardColumns() {
+        return boardColumns;
+    }
+
+    public void setBoardColumns(List<BoardColumnEntity> boardColumns) {
+        this.boardColumns = boardColumns;
+    }
+
+    
+
+
+}
