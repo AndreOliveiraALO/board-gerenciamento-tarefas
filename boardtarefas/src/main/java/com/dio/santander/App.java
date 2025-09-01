183d7509b47211f51c2423c41336456c0a7018c8
André Luiz de Oliveira <andreloliveira28@gmail.com>
2025-08-31 23:30:46 -0300

    criação da classes da camada ui

--- boardtarefas/src/main/java/com/dio/santander/App.java
+++ boardtarefas/src/main/java/com/dio/santander/App.java
@@ -4,14 +4,16 @@ import java.sql.SQLException;
 
 import static com.dio.santander.persistence.config.ConnectionConfig.getConnection;
 import com.dio.santander.persistence.migration.MigrationStrategy;
+import com.dio.santander.ui.MainMenu;
 
 public class App 
 {
     public static void main( String[] args ) throws SQLException
     {
         try(var connection = getConnection()){
-            new MigrationStrategy(connection).executeMigration();
-            System.out.println("Migrações executadas com sucesso!");
-        }  
+            new MigrationStrategy(connection).executeMigration();            
+        }   
+        new MainMenu().execute();
     }
+
 }
